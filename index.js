29ad8dee6078fea29dab7ce55925f035ca913c53
alfazachmmosi <zalfa3890@gmail.com>
2024-03-19 21:13:43 +0300

    commit

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function introductionWithLanguageOptional(name = "Alfa", language = "JavaScript"
     console.log(`Hello, ${name}! I am learning ${language}.`);
   }
   
-
+  introductionWithLanguageOptional();
   
